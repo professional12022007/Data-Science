9a2c71e2e7e012436129ebf35c6667a0abb344de
Divyansh Gupta <divyanshvinu12@gmail.com>
2026-06-05 13:10:05 +0530

    3rd class of python

---
+++ Class/3.py
@@ -0,0 +1,69 @@
+# # lambda parameter: expression
+# square = lambda x: x * x
+# print(square(5))  # 25
+
+# add = lambda a, b: a + b
+
+# print(add(10, 20))  # 30
+
+# maximum = lambda a, b: a if a > b else b
+
+# print(maximum(7, 3))  # 7
+# # string function 
+# text = "hello world"
+
+# # Convert all letters to uppercase
+# print(text.upper())
+
+# # Convert all letters to lowercase
+# print(text.lower())
+
+# # Capitalize first character only
+# print(text.capitalize())
+# text = "  e7tfgkasdbkajahf.  "
+
+# # Remove spaces from both ends
+# print(text)
+# print(text.strip())
+# text = "I like Java"
+
+# # Replace Java with Python
+# print(text.replace("Java", "Python"))
+
+# first="Harsh Surana   "
+# second="      jara ghum ke dikhana  "
+# first1=first.strip()
+# second1=second.strip()
+# print(first1+" "+second1)
+
+# a=" today's"
+# b="   weather    "
+# c="is GooD"
+# vass=a.strip()
+# vass1=vass.capitalize()
+# vass=b.strip()
+# vass2=vass1+" "+ vass
+# vass=c.lower()
+# vass1=vass2+" "+vass
+# print(vass1)
+
+
+# print(vass1.replace("good", "bad"))
+
+
+# print(vass1.find("good"))
+
+# print(vass1.startswith("Toda"))
+# print(vass1.endswith("good"))
+
+
+
+# # set 
+st={2,3,4,656,7687,97,54,32,32,2}
+print(st)
+dct={
+    "name":"ramesh",
+    "age":32,
+    "occupation":"helper"
+}
+print(dct["name"])
